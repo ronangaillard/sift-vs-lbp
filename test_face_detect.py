5b8fb50548ce85377b8fb71db429f16671048a50
Guilhem Codron <guilhemcodron@gmail.com>
2017-02-27 12:28:42 +0100

    added face detection

---
+++ test_face_detect.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 
+from matplotlib import pyplot as plt
+import os
+
+OPENCV_PATH = '/usr/local/Cellar/opencv3/HEAD-dcbed8d/share/OpenCV/haarcascades/'
+
+face_cascade = cv2.CascadeClassifier(OPENCV_PATH + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(OPENCV_PATH + 'haarcascade_eye.xml')
+
+i = 0
+for root, dirs, files in os.walk('./photos'):
+    data = [os.path.join(root,f) for f in files]
+    for fi in data:
+        img = cv2.imread(fi)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            crop_img = img[y:y+h, x:x+w]
+            cv2.imwrite('gui' + str(i) +'.jpg', crop_img)
+            i+=1
