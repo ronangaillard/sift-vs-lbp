a3b6c7d5c8460b21146808104be52796721f4724
Ronan Gaillard <ronan.gaillard@live.fr>
2017-02-27 13:15:17 +0100

    Update train script

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from scipy.cluster.vq import *
 import numpy as np
 import glob
 import cv2
+import scipy
 from test_face_detect import PROCESSED_FACES_DIR
 
 # Creating list of images 
@@ -24,6 +25,17 @@ test = my_set[len(my_set)/2:]
 # Compute descriptors
 print 'Computing descriptors'
 sift = cv2.xfeatures2d.SIFT_create()
-descriptors = [sift.detectAndCompute(pic, None) for pic in train]
+
+descriptors = np.array([])
+for pic in train:
+    kp, des = sift.detectAndCompute(pic, None)
+    descriptors = np.append(descriptors, des)
+
+desc = np.reshape(descriptors, (len(descriptors)/128, 128))
+desc = np.float32(desc)
+
+print desc
+
+print "kmeans", scipy.cluster.vq.kmeans(desc, k_or_guess=1000, iter=20, thresh=1e-05)
 
 
